0098fb809fb54a277cc158bcb9f68d90009dce12
CalzoneB <ko_king_1@hotmail.com>
2026-01-20 01:18:09 +0000

    Revert "Fix start match slash"
    This reverts commit 84c2510f6d796cd281c55aac34854f77b0bf5358.

--- public/commands/tournamentCommands/startMatch.js
+++ public/commands/tournamentCommands/startMatch.js
@@ -3,7 +3,6 @@ const { SlashCommandBuilder } = require("discord.js");
 const fs = require("fs");
 const sleep = require("util").promisify(setTimeout);
 eval(fs.readFileSync("./public/main.js") + "");
-eval(fs.readFileSync("./public/tournament/tournamentFunctions.js") + "");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -11,26 +10,9 @@ module.exports = {
     .setDescription("If there is a tournament set up, we can manually start a days worth of matches using this command."),
   async execute(interaction) {
     await interaction.reply("Sending Messages");
-    const previousMatches = await EndMatches(interaction);
-
-    const db = GetDb();
-    db.read();
-    const currentTournamentName = await getCurrentTournament(db);
-    const tournamentDetails = await db.get("tournaments").nth(0).value();
-    if (!tournamentDetails || currentTournamentName === "N/A") {
-      return interaction.editReply(
-        "There doesn't appear to be a tournament running at this time."
-      );
-    }
-    const tournamentDb = tournamentDetails[currentTournamentName];
-    const matchesPerDay = tournamentDb?.roundsPerTurn || 1;
-
-    for (let i = 0; i < matchesPerDay; i++) {
-      await StartMatch(interaction, "", i > 0, i === 0 ? previousMatches : []);
-      if (i < matchesPerDay - 1) {
-        await sleep(15000);
-      }
-    }
+    await startMatch(interaction);
+    await sleep(15000);
+    await startMatch(interaction, "", true);
     //await interaction.followup.editReply("ok!");
   },
 };
